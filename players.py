830281bbb1ef8ef5d50b6e3029ca5726857d4a77
nikolata <npmetodiev@gmail.com>
2020-03-25 21:19:02 +0200

    architecture added, small change on players.py

--- players.py
+++ players.py
@@ -3,6 +3,8 @@ class Player:
 	def __init__(self,*,name):
 		assert type(name) == str, 'TypeError'
 		self.name = name
+		self.cards = []
+		self.announcements = {'belot':['s','d'],'terca':['K']}
 
 
 	def set_cards(self, cards):
