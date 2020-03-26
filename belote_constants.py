9bce747a4426103da27652c88792f77200433b6d
dzhelek <yoan.dzhelekarski@gmail.com>
2020-03-26 13:58:30 +0200

    Make constants human-readable

--- belote_constants.py
+++ belote_constants.py
@@ -1,3 +1,10 @@
 BELOTE_ALL_CARD_VALUES = ['7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 BELOTE_ALL_CARD_COLORS = ['s', 'd', 'h', 'c']
-BELOTE_ALL_CARDS = ['7s', '7d', '7h', '7c', '8s', '8d', '8h', '8c', '9s', '9d', '9h', '9c', '10s', '10d', '10h', '10c', 'Js', 'Jd', 'Jh', 'Jc', 'Qs', 'Qd', 'Qh', 'Qc', 'Ks', 'Kd', 'Kh', 'Kc', 'As', 'Ad', 'Ah', 'Ac']
+BELOTE_ALL_CARDS = ['7s', '7d', '7h', '7c',\
+                    '8s', '8d', '8h', '8c',\
+                    '9s', '9d', '9h', '9c',\
+                    '10s', '10d', '10h', '10c',\
+                    'Js', 'Jd', 'Jh', 'Jc',\
+                    'Qs', 'Qd', 'Qh', 'Qc',
+                    'Ks', 'Kd', 'Kh', 'Kc',
+                    'As', 'Ad', 'Ah', 'Ac']
