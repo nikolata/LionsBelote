cc950da26602d28655ddb485614a85f57a23cbfc
nikolata <npmetodiev@gmail.com>
2020-03-30 12:08:55 +0300

    main changes

--- game.py
+++ game.py
@@ -12,12 +12,12 @@ class Game:
 	def play_round(self,round_num = 1):
 		r = Round(round_num,self.team1,self.team2)
 		if self.last_winner == 'team1':
-			r.play(player = random.choice([team1.player1,team1.player1]))
+			r.play(player = random.choice([self.team1.player1,self.team1.player1]))
 		elif self.last_winner == 'team2':
-			r.play(player = random.choice([team2.player1,team2.player1]))
+			r.play(player = random.choice([self.team2.player1,self.team2.player1]))
 		else:
-			r.play(player = team1.player1)
+			r.play(player = self.team1.player1)
 
 
-	def set_dict(self, round):
-		self.dict.append(round.number: round.set_dict())
+	#def set_dict(self, round):
+	#	self.dict.append(round.number: round.set_dict())
--- main.py
+++ main.py
@@ -15,11 +15,12 @@ def main():
 	p3 = Player(name = team1_players[1].replace(' ',''))
 
 	p2 = Player(name = team2_players[0].replace(' ',''))
-	p4 = Player(name = team4_players[1].replace(' ',''))
+	p4 = Player(name = team2_players[1].replace(' ',''))
 
 	team1 = Team('Mashinite',p1,p3)
 	team2 = Team('Slabacite',p2,p4)
 #test
+
 	game = Game(team1,team2)
 	while game.team1_wins !=2 and game.team2_wins!=2:
 		round_number = 1
@@ -31,10 +32,19 @@ def main():
 					safe_to_txt(team1,team2,first_round = True)
 				else:
 					safe_to_txt(team1,team2)
-				if sum(team1.points)>150 or sum(team1.points)>150 and sum(team1.points) != sum(team2.poits):
+				if sum(team1.points)>150 or sum(team2.points)>150 and sum(team1.points) != sum(team2.points):
 					is_game_won = True
+				print('-----------------')
+				print(team1.points)
+				print(team1.valid_announcements_for_round)
+				print('-----------------')
+				print(team2.points)
+				print(team1.valid_announcements_for_round)
+
+				print('-----------------')
+
 				round_number +=1
-		safe_to_txt(team1,team2,final_round = True)
+		safe_to_txt(team1,team2,last_round = True)
 		if team1.points > team2.points:
 			game.team1_wins +=1
 			game.last_winner = 'team1'
