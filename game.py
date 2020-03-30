2d0de6ec8e3a899dea3eecc526c5f10f61b3700e
nikolata <npmetodiev@gmail.com>
2020-03-30 04:09:47 +0300

    main and game changes

--- game.py
+++ game.py
@@ -7,15 +7,16 @@ class Game:
 		self.team2 = team2
 		self.team1_wins = 0
 		self.team2_wins = 0
-		self.round = 1
+		self.last_winner = None
 		self.dict = {}
-	def play_round(self,last_winner = None):
-		if last_winner = team1:
-			round.play(player = random.choice([team1.player1,team1.player1]))
-		elif last_winner = team2:
-			round.play(player = random.choice([team2.player1,team2.player1]))
+	def play_round(self,round_num = 1):
+		r = Round(round_num,self.team1,self.team2)
+		if self.last_winner == 'team1':
+			r.play(player = random.choice([team1.player1,team1.player1]))
+		elif self.last_winner == 'team2':
+			r.play(player = random.choice([team2.player1,team2.player1]))
 		else:
-			round.play(player = team1.player1)
+			r.play(player = team1.player1)
 
 
 	def set_dict(self, round):
