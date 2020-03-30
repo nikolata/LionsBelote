4a3996ab543c0c74ea3e779c1225b5fab15119ea
nikolata <npmetodiev@gmail.com>
2020-03-30 11:47:40 +0300

    main changes

--- main.py
+++ main.py
@@ -24,20 +24,24 @@ def main():
 	while game.team1_wins !=2 and game.team2_wins!=2:
 		round_number = 1
 		create_file_with_headers(team1,team2)
-		while not sum(team1.points)>150 or not sum(team1.points)>150:
+		is_game_won = False
+		while not is_game_won:
 				game.play_round(round_number)
 				if round_number == 1:
 					safe_to_txt(team1,team2,first_round = True)
 				else:
 					safe_to_txt(team1,team2)
+				if sum(team1.points)>150 or sum(team1.points)>150 and sum(team1.points) != sum(team2.poits):
+					is_game_won = True
 				round_number +=1
 		safe_to_txt(team1,team2,final_round = True)
 		if team1.points > team2.points:
 			game.team1_wins +=1
 			game.last_winner = 'team1'
-		else:
+		elif team1.points<team2.points:
 			game.team2_wins +=1
 			game.last_winner = 'team2'
+
 		display_game_points(game.team1_wins,game.team2_wins)
 
 if __name__ == '__main__':
